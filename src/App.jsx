5349151b304e53b2c602491a096370c21c919a9d
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-07-26 10:25:42 -0700

    Move all routes into AppRoutes, delete App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
-import NotFound from './components/pages/NotFound';
-import Login from './components/auth/Login.jsx';
-import Register from './components/auth/Register.jsx';
-import SearchBar from './components/search/SearchBar.jsx';
-import PaymentForm from './components/payment/PaymentForm.jsx';
-import './App.css';
 
+import AppRoutes from './AppRoutes';
 
 const App = () => {
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/search" element={<SearchBar />} />
-        <Route path="/payment" element={<PaymentForm />} />
-      </Routes>
-    </BrowserRouter>
+    <AppRoutes/>
   );
 };
 
