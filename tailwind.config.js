79d6899b6348898c1ac5ca98c00547f4b6326e86
Valentina <vrudnitskaya@gmail.com>
2024-07-26 16:33:23 -0500

    add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#262626",
+        white: "#F5FFFF",
+        red: "#F95C7B",
+        lightGreen: "#8ADADD",
+        darkGreen: "#10BEB8",
+        yellow: "#F9B146",
+        lightBlue: "#F0FFFF",
+      },
+      fontFamily: {
+        spartan: ["League Spartan", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 });
