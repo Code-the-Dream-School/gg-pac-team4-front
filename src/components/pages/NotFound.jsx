c94bccee75bfa2b06da512e5d68a0f36fe0efdfd
Valentina <vrudnitskaya@gmail.com>
2024-07-28 16:45:27 -0500

    change the image, bg color, add h1

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import img from '../../assets/notFound.png';
+import img from '../../assets/imgPage404.jpg';
 
 const NotFound = () => {
   return (
-    <div className="bg-lightGreen w-full h-screen flex flex-col items-center pt-5">
-      <img src={img} alt="Error 404" height="350px"/>
-      <Link className="text-white font-medium text-2xl font-spartan" to="/">Back to main page</Link>
+    <div className="bg-pureWhite w-full h-screen flex flex-col gap-7 items-center mt-24">
+      <h1 className="text-red font-bold text-2xl sm:text-4xl font-spartan uppercase">Page not found</h1>
+      <img src={img} alt="Error 404" className="w-[350px] sm:w-[550px]"/>
+      <Link className="text-black font-medium text-xl sm:text-2xl font-spartan" to="/">Back to main page</Link>
     </div>
   );
 };
