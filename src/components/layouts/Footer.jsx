3fb88dce9d5630ba6158b7272ec88da32ce21c8a
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-07-29 12:12:53 -0700

    add footer component to Home Page

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center bg-lightGreen w-full h-24 text-center text-black">
+      <p>CTD Practicum Team 4</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-
+import Footer from "../layouts/Footer"
 const HomePage = () => {
 
   const navigate = useNavigate();
@@ -15,13 +15,18 @@ const HomePage = () => {
 
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+   <main className="flex-grow p-4">
       <h1 className="text-3xl font-spartan font-bold">HOME PAGE</h1>
       <button onClick={search}>Search classes</button>
       <button onClick={login}>Log In</button>
       <button className='bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-lg py-1 px-7 rounded-lg' 
               onClick={register}>Join</button>
-    </>
+        </main>
+        <Footer />
+    </div>
+ 
+    
   );
 };
 
