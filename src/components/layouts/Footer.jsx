8edc2e8df104b857b1363a11dce96a095ef2dc1d
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-07-29 14:49:34 -0700

    move Footer component to AppRoutes

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex w-full flex-row flex-wrap items-center justify-center bg-lightGreen w-full h-24 text-center text-black">
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center bg-lightGreen w-full h-24 text-center text-black bottom-0">
       <p>CTD Practicum Team 4</p>
     </footer>
   );
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-import Footer from "../layouts/Footer"
+import { useNavigate } from "react-router-dom";
 const HomePage = () => {
-
   const navigate = useNavigate();
   const search = () => {
     navigate("/search");
@@ -13,20 +11,18 @@ const HomePage = () => {
     navigate("/register");
   };
 
-
   return (
-    <div className="flex flex-col min-h-screen">
-   <main className="flex-grow p-4">
+   <>
       <h1 className="text-3xl font-spartan font-bold">HOME PAGE</h1>
       <button onClick={search}>Search classes</button>
       <button onClick={login}>Log In</button>
-      <button className='bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-lg py-1 px-7 rounded-lg' 
-              onClick={register}>Join</button>
-        </main>
-        <Footer />
-    </div>
- 
-    
+      <button
+        className="bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-lg py-1 px-7 rounded-lg"
+        onClick={register}
+      >
+        Join
+      </button>
+    </>
   );
 };
 
