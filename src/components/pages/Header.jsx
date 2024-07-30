5dbcf5d4e6b7d7375450c1029601ad8c002206c2
Valentina <vrudnitskaya@gmail.com>
2024-07-29 21:15:11 -0500

    edit header to make it responsive, add effect to hide some its items

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,9 +1,40 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
+import closeIcon from '../../assets/icons/close.png';
 import loginIcon from '../../assets/icons/login.png';
+import logoutIcon from '../../assets/icons/logout.png';
+import navIcon from '../../assets/icons/nav.png';
 import searchIcon from '../../assets/icons/search.png';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+    
+    const location = useLocation();
+    useEffect(() => {
+        if (location.pathname === "/search") {
+            document.querySelector('form').style.visibility = "hidden";
+        } else{
+            document.querySelector('form').style.visibility = "visible";
+        }
+        
+        if(location.pathname === "/login") {
+            document.querySelector('#btnLogin').style.visibility = "hidden";
+        } else {
+            document.querySelector('#btnLogin').style.visibility = "visible";
+        }
+        
+        if(location.pathname === "/register") {
+            document.querySelector('#btnJoin').style.display = "none";
+        } else {
+            document.querySelector('#btnJoin').style.display = "block";
+        }
+    }, [location.pathname]);
+
     const navigate = useNavigate();
     const search = () => {
         navigate("/search");
@@ -17,39 +48,48 @@ const Header = () => {
 
     return (
         <header className="h-[80px] w-full flex items-center px-11 justify-between">
-            <Link className="text-black font-spartan font-semibold text-xl sm:text-3xl w-1/3">TalentStudio</Link>
-            
-                <div className="w-2/3 hidden relative py-6 sm:flex justify-between">
-                    <form className="border-2 border-red rounded-full h-10 w-1/3 flex justify-between items-center px-4 ml-6">
-                        <input className="focus:outline-none w-2/3" />
-                        <button onClick={search}><img src={searchIcon} className="h-[24px]" /></button>
-                    </form>
-                    <div className="flex gap-4 items-center">
-                        <button className="font-spartan font-semibold text-base sm:text-lg flex items-center "
-                            onClick={login}>
-                            <img src={loginIcon} className="h-[15px] sm:h-[20px]" />
-                            Log In
-                        </button>
-                        <button className='bg-red hover:bg-pureWhite hover:text-red w-16 h-8 hover:border-2 hover:border-red text-white font-spartan font-semibold text-base sm:text-lg rounded-lg transition duration-300 easy-in'
-                            onClick={register}>Join
-                        </button>
-                    </div>
+            <Link className="text-black font-spartan font-semibold text-xl sm:text-3xl w-1/3" to="/">TalentStudio</Link>
+            {}
+            {/* Nav for the big screens */}
+            <nav className="w-2/3 hidden relative py-6 sm:flex justify-between">
+                <form className="border-2 border-red rounded-full h-10 w-2/4 flex justify-between items-center px-4 ml-6 gap-1">
+                    <input className='focus:outline-none w-2/3' placeholder="Search online classes or teachers..." />
+                    <button onClick={search}><img src={searchIcon} className="h-[24px]" /></button>
+                </form>
+                <div id="mainNav" className="flex gap-4 items-center">
+                    <button id="btnLogin" className="font-spartan font-semibold text-base sm:text-lg flex items-center "
+                        onClick={login}>
+                        <img src={loginIcon} className="h-[15px] sm:h-[20px]" />
+                        Log In
+                    </button>
+                    <button id="btnJoin" className='bg-red hover:bg-pureWhite hover:text-red w-16 h-8 hover:border-2 hover:border-red text-white font-spartan font-semibold text-base sm:text-lg rounded-lg transition duration-300 easy-in'
+                        onClick={register}>Join
+                    </button>
                 </div>
+            </nav>
 
-            {/* 
-            <form className="border-2 border-red rounded-full w-1/3 h-10 flex justify-between px-4">
-                <input className="focus:outline-none hidden sm:block" placeholder="Search for classes..." />
-                <button onClick={search}><img src={searchIcon} className="h-[15px] sm:h-[20px]" /></button>
-            </form>
-            <div className="flex gap-4">
-                <button className="font-spartan font-semibold text-base sm:text-lg flex items-center "
-                    onClick={login}>
-                    <img src={loginIcon} className="h-[15px] sm:h-[20px]" />
-                    Log In
-                </button>
-                <button className='bg-red hover:bg-pureWhite hover:text-red w-16 h-8 hover:border-2 hover:border-red text-white font-spartan font-semibold text-base sm:text-lg rounded-lg transition duration-300 easy-in'
-                    onClick={register}>Join</button>
-            </div> */}
+            {/* Nav for the small screens */}
+            
+            <nav className="sm:hidden relative flex flex-row my-4 gap-2">
+                <button onClick={search}><img src={searchIcon} className="h-[24px]" /></button>
+                <button onClick={handleToggle}>{isOpen ? <img src={closeIcon} className="h-[24px]" /> : <img src={navIcon} className="h-[24px]" />}</button>
+                <ul className={
+                    isOpen
+                        ? 'absolute right-0 top-10 w-[70px] flex flex-col items-end gap-1'
+                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                }>
+                    <li className="hover:font-semibold hover:underline"><Link to="/login">Log In</Link></li>
+                    <li className="hover:font-semibold hover:underline"><Link to="/register">Join</Link></li>
+                </ul>
+            </nav>
+
+            {/* Nav for the account*/}
+            {/* currently there is no route for it*/}
+            {/* <nav className="sm:hidden relative flex flex-row my-4 gap-2 items-center">
+                <div id="user-photo" className="h-[40px] w-[40px] rounded-full bg-lightGreen"></div>
+                <p className="font-spartan text-lg font-semibold">User name</p>
+                <Link to="/"><img src={logoutIcon} className="h-[24px]" /></Link>
+            </nav> */}
         </header>
     );
 };
