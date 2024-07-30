5798432877ea47beef53221ee92d122e3e9a4880
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-07-29 19:36:00 -0700

    add first section of main page

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,27 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import mainImage from '../../assets/imgMainPage.jpg'
 
 const HomePage = () => {
-
   const navigate = useNavigate();
-  const search = () => {
-    navigate("/search");
-  };
+
   const login = () => {
     navigate("/login");
   };
+
   const register = () => {
     navigate("/register");
   };
 
-
   return (
-    <>
-      <h1 className="text-3xl font-spartan font-bold">HOME PAGE</h1>
-      <button onClick={search}>Search classes</button>
-      <button onClick={login}>Log In</button>
-      <button className='bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-lg py-1 px-7 rounded-lg' 
-              onClick={register}>Join</button>
-    </>
+    <div className="flex space-between px-20 py-10">
+      <div className="pt-36 pr-28">
+        <h1 className="text-7xl font-spartan font-semibold text-left mb-8">Helping unlock<br /> your talent with<br /> the best teachers</h1>
+        <button
+          className="bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-lg py-1 px-7 rounded-lg"
+          onClick={register}
+        >
+          Get started
+        </button>
+      </div>
+      <div>
+      <img src={mainImage} alt="" className="" />
+      </div>
+    </div>
   );
 };
 
