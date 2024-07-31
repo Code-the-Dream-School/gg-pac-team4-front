189ea185dbc20d9c5eced4a15ded5ee2a4c12b61
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-07-30 17:54:07 -0700

    removed flex-wrap, text-center and bottom0

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex w-screen  flex-row flex-wrap items-center justify-center bg-lightGreen w-full h-24 text-center text-black bottom-0">
+    <footer className="flex w-screen  flex-row items-center justify-center bg-lightGreen w-full h-24  text-black">
       <p>CTD Practicum Team 4</p>
     </footer>
   );
