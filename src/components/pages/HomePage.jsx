2385768894f91d51b9f99ddf87aa2370c1b34621
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-08-01 21:31:44 -0700

    added fixes to width of text and margins

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import mainImage from "../../assets/imgMainPage.jpg";
 import GreenBlobImage from "../../assets/blob_green.png";
 import YellowBlobImage from "../../assets/blob_yellow.png";
 import PurpleBlobImage from "../../assets/blob_dusty_purple.png";
-import items from "../../data/data"
+import items from "../../data/data";
 import ArrowUp from "../../assets/icons/icons-arrow-up.png";
 import ArrowSearch from "../../assets/icons/icon-search.svg";
 
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 };
 
@@ -23,91 +23,93 @@ const HomePage = () => {
   };
 
   return (
-    <div className="flex flex-col my-auto lg:mx-auto my-12">
+    <div className="flex flex-col w-10/12 my-auto lg:mx-auto mt-4 md:mt-12 my-12">
       <section className="flex flex-col lg:flex-row lg:justify-between xl:justify-between justify-center items-center">
-        <div className="lg:text-left text-center items-center justify-center xl:justify-between">
+        <div className="lg:text-left text-center lg:w-2/5 items-center justify-center xl:justify-between">
           <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-4xl xl:text-5xl items-center font-spartan font-semibold tracking-wide leading-tight pt-3 mb-8">
-            Helping unlock
-            <br />
-            your talent with
-            <br />
-            the best teachers
+            Helping unlock your talent with the best teachers
           </h1>
           <div className="flex justify-center lg:justify-start">
-          <button
-            className="flex items-center bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-2xl py-4 px-8 rounded-lg"
-            onClick={search}
-          >
-            Get started
-            <img src={ArrowSearch} alt="Arrow Right" className="ml-4 w-6 h-6" />
-          </button>
+            <button
+              className="flex items-center bg-red hover:bg-pureWhite hover:text-red hover:border-2 hover:border-red text-white font-spartan font-semibold text-2xl py-4 px-8 rounded-lg"
+              onClick={search}
+            >
+              Get started
+              <img
+                src={ArrowSearch}
+                alt="Arrow Right"
+                className="ml-4 w-6 h-6"
+              />
+            </button>
           </div>
-         
         </div>
 
-        <div className="m-10">
-          <img className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl pt-0 lg:pt-20 xl:pt-0" src={mainImage} alt="art skills image" />
+        <div className="mt-10 lg:mt-0 xl:mt-0">
+          <img
+            className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl pt-0 lg:pt-20 xl:pt-0"
+            src={mainImage}
+            alt="art skills image"
+          />
         </div>
       </section>
       <section className="flex flex-col lg:flex-row items-center justify-center">
-      <div
-        className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
-        style={{ 
-          backgroundImage: `url(${GreenBlobImage})`
-        }}
-      >
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl font-spartan font-semibold text-white text-center">
-            Courses for
-            <br /> every age
-          </div>
-        </div>
-      </div>
         <div
-        className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${YellowBlobImage})` }}
-      >
+          className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url(${GreenBlobImage})`,
+          }}
+        >
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl font-spartan font-semibold text-white">
-              Over 100
-              <br /> subject taught
+            <div className="w-4/12 md:w-1/4 text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl  font-spartan font-semibold text-white text-left">
+              Courses for every age
             </div>
           </div>
         </div>
-        
         <div
-        className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${PurpleBlobImage})` 
-      }}
-      >
+          className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${YellowBlobImage})` }}
+        >
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl font-spartan font-semibold text-white text-left pl-10">
-              1000+
-              <br />
-              experienced teachers
+            <div className="w-2/5 md:w-1/3 text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl font-spartan font-semibold text-white">
+              Over 100 subject taught
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="relative w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${PurpleBlobImage})` }}
+        >
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-3/5 md: w-5/12 text-2xl lg:text-2xl xl:text-2xl md:text-3xl sm:text-2xl font-spartan font-semibold text-white text-left pl-10">
+              1000+ experienced teachers
             </div>
           </div>
         </div>
       </section>
-      <section className="px-4 py-8">
+      <section className="relative px-4 py-8">
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
             {items.map((item, index) => (
-              <div key={index} className="flex items-center text-center-6 rounded-lg">
+              <div
+                key={index}
+                className="flex items-center text-center-6 rounded-lg"
+              >
                 <img src={item.src} alt={item.alt} className="mr-2" />
-                <p className="text-2xl font-medium hover:text-darkGreen hover:cursor-pointer">{item.text}</p>
+                <p className="text-2xl font-spartan font-medium hover:text-darkGreen hover:cursor-pointer">
+                  {item.text}
+                </p>
               </div>
             ))}
           </div>
         </div>
         <button
-          className="absolute right-4 bg-darkGreen rounded-full w-12 h-12 flex items-center justify-center shadow-lg hover:bg-lightGreen"
+          className="absolute right-2 bottom-8 bg-darkGreen rounded-full w-12 h-12 flex items-center justify-center shadow-lg hover:bg-lightGreen"
           onClick={scrollToTop}
         >
           <img src={ArrowUp} alt="Arrow Up" />
         </button>
       </section>
-    
     </div>
   );
 };
