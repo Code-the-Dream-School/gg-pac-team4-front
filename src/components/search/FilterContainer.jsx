e6621d7d8be7c1f1ff762acc6814f434e472fe86
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-08-16 12:11:43 -0700

    add responsive for filter container

--- src/components/search/FilterContainer.jsx
+++ src/components/search/FilterContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import iconsArrowDown from '../../assets/icons/icon.svg';
 import iconsSort from '../../assets/icons/icon-sort.svg';
-//filter functionality will work later
+//filter functionality will work after backend will fin
 const buttons = [
   { label: 'Specialties', filter: 'specialties', icon: iconsArrowDown },
   { label: 'Age', filter: 'age', icon: iconsArrowDown },
@@ -13,11 +13,11 @@ const buttons = [
 
 const FilterButtons = () => {
   return (
-    <div className="flex flex-row justify-around items-center pb-6 bg-lightGreen w-full">
+    <div className="flex flex-wrap justify-around items-center px-4 pb-6 gap-4 bg-lightGreen w-full">
       {buttons.map(({ label, icon }) => (
         <button
           key={label}
-          className="bg-pureWhite rounded-lg font-roboto text-black text-xl p-2 flex items-center"
+          className="bg-pureWhite rounded-lg font-roboto text-black text-xl p-2 flex items-center w-auto"
         >
           <span>{label}</span>
           <img src={icon} className="h-6 mx-3" alt={`${label} icon`} />
