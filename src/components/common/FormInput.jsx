e3954c1265bc3d90fda7cf1487fc0daad19904d7
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-21 16:56:10 -0700

    remove z-10 class from label

--- src/components/common/FormInput.jsx
+++ src/components/common/FormInput.jsx
@@ -22,7 +22,7 @@ const FormInput = ({
         />
         <label
           htmlFor={id}
-          className={`absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] ${disabled ? 'bg-lightGray text-darkGray' : 'bg-pureWhite text-black'} px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}
+          className={`absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] ${disabled ? 'bg-lightGray text-darkGray' : 'bg-pureWhite text-black'} px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}
         >
           {children}
         </label>
