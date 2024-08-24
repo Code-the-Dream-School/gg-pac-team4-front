3b17dad6932d6ab6c7114bf3b815766d50c792d8
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-23 21:46:56 -0700

    move form rendering into a separate component RegisterForm.jsx

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -3,9 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import { registerStudent, registerTeacher } from '../../util/DataBaseRequests';
-import FormInput from '../common/FormInput';
-import FormSubmitBtn from '../common/FormSubmitBtn';
-import FormErrorMsg from '../common/FormErrorMsg';
+import RegisterForm from './RegisterForm';
 
 const Register = () => {
   const [userRole, setUserRole] = useState('student');
@@ -119,14 +117,12 @@ const Register = () => {
       setAdultNameError('');
     }
 
-    if (dateOfBirth == '') {
+    if (dateOfBirth == '' && userRole === 'student') {
       setDateOfBirthError('Date of birth is required');
       isDataValid = false;
     } else {
       setDateOfBirthError('');
     }
-
-
 
     return isDataValid;
   }
@@ -238,124 +234,36 @@ const Register = () => {
         <Link to="/login" className="underline">Log In</Link>
       </p>
 
-      {userRole === 'student' ? (
-        <div className="flex w-full font-spartan font-medium text-3xl text-grey text-center mb-5">
-          <button className="basis-2/4 text-black border-b-[3px] border-black">as a Student</button>
-          <button className="basis-2/4 border-b-[3px] border-grey" onClick={switchUserRole}>as a Teacher</button>
-        </div>
-      ) : (
-        <div className="flex w-full font-spartan font-medium text-3xl text-grey text-center mb-5">
-          <h2 className="basis-2/4 border-b-[3px] border-grey" onClick={switchUserRole}>as a Student</h2>
-          <h2 className="basis-2/4 text-black border-b-[3px] border-black">as a Teacher</h2>
-        </div>
-      )}
-
-
-      <form className="w-full px-2" onSubmit={handleUserRegistration}>
-        <div className="flex w-full gap-x-4 items-end">
-          <div className="basis-2/4">
-            {firstNameError && <FormErrorMsg error={firstNameError} />}
-            <FormInput
-              type="text"
-              id="first-name"
-              label="first-name"
-              value={firstName}
-              onChange={handleFirstNameChange}
-              placeholder=" "
-              className="basis-2/4">
-              First Name
-            </FormInput>
-          </div>
-          <div className="basis-2/4">
-            {lastNameError && <FormErrorMsg error={lastNameError} />}
-            <FormInput
-              type="text"
-              id="last-name"
-              label="last-name"
-              value={lastName}
-              onChange={handleLastNameChange}
-              placeholder=" "
-              className="basis-2/4">
-              Last Name
-            </FormInput>
-          </div>
-        </div>
-        {userRole === 'student' && (
-          <>
-            <div className="flex w-full gap-x-4 items-end">
-              <div className="basis-2/4">
-                {dateOfBirthError && <FormErrorMsg error={dateOfBirthError} />}
-                <FormInput
-                  type="date"
-                  id="dob"
-                  label="dob"
-                  value={dateOfBirth}
-                  onChange={handleDateOfBirthChange}
-                  placeholder=" "
-                >
-                  Date of Birth
-                </FormInput>
-              </div>
-              <div className="basis-2/4">
-                {adultNameError && <FormErrorMsg error={adultNameError} />}
-                <FormInput
-                  type="text"
-                  id="adult-name"
-                  label="adult-name"
-                  value={adultName}
-                  onChange={handleAdultNameChange}
-                  placeholder=" "
-                  disabled={!isAdultNameRequired}>
-                  Adult Full Name
-                </FormInput>
-              </div>
-            </div>
-          </>
-        )}
-        {emailError && <FormErrorMsg error={emailError} />}
-        <FormInput
-          type="email"
-          id="email"
-          label="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder=" ">
-          Email Address
-        </FormInput>
-        {confirmEmailError && <FormErrorMsg error={confirmEmailError} />}
-        <FormInput
-          type="email"
-          id="email2"
-          label="email2"
-          value={confirmEmail}
-          onChange={handleConfirmEmailChange}
-          placeholder=" ">
-          Confirm Email Address
-        </FormInput>
-        {passwordError && <FormErrorMsg error={passwordError} />}
-        <FormInput
-          type="password"
-          id="password"
-          label="password"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder=" ">
-          Password
-        </FormInput>
-        {confirmPasswordError && <FormErrorMsg error={confirmPasswordError} />}
-        <FormInput
-          type="password"
-          id="password2"
-          label="password2"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          placeholder=" ">
-          Confirm Password
-        </FormInput>
-        <FormSubmitBtn
-          value={`Create ${userRole} account`}
-        />
-      </form>
+      <RegisterForm
+        onSubmit={handleUserRegistration}
+        userRole={userRole}
+        switchUserRole={switchUserRole}
+        firstName={firstName}
+        firstNameError={firstNameError}
+        handleFirstNameChange={handleFirstNameChange}
+        lastName={lastName}
+        lastNameError={lastNameError}
+        handleLastNameChange={handleLastNameChange}
+        dateOfBirth={dateOfBirth}
+        dateOfBirthError={dateOfBirthError}
+        handleDateOfBirthChange={handleDateOfBirthChange}
+        adultName={adultName}
+        adultNameError={adultNameError}
+        handleAdultNameChange={handleAdultNameChange}
+        isAdultNameRequired={isAdultNameRequired}
+        email={email}
+        emailError={emailError}
+        handleEmailChange={handleEmailChange}
+        confirmEmail={confirmEmail}
+        confirmEmailError={confirmEmailError}
+        handleConfirmEmailChange={handleConfirmEmailChange}
+        password={password}
+        passwordError={passwordError}
+        handlePasswordChange={handlePasswordChange}
+        confirmPassword={confirmPassword}
+        confirmPasswordError={confirmPasswordError}
+        handleConfirmPasswordChange={handleConfirmPasswordChange}
+      />
 
       <Modal
         isOpen={isModalOpen}
