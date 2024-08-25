6563317e5efc22e363fa4683f58b61d18a4166c8
mariyador <aniknorod@gmail.com>
2024-08-25 10:30:13 -0400

    socket installed. user connected

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import AppRoutes from './AppRoutes';
 import { AuthProvider } from './AuthProvider';
 
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8000')
+
 const App = () => {
   return (
     <AuthProvider>
