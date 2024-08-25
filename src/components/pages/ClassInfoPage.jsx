54fc71ff2a193e5adfd6c447e0ec497e479a1fdc
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-08-25 00:33:08 -0700

    add teacher info and layout

--- src/components/pages/ClassInfoPage.jsx
+++ src/components/pages/ClassInfoPage.jsx
@@ -6,27 +6,33 @@ import Loader from '../common/Loader';
 import AgeIcon from '../../assets/icons/icon-age.svg';
 import LessonTypeIcon from '../../assets/icons/icon-lesson.svg';
 import ScheduleIcon from '../../assets/icons/icons-schedule.svg';
+import IconClock from '../../assets/icons/icon-clock.png';
+import IconDegree from '../../assets/icons/icons-degree.png';
 
 const ClassInfoPage = () => {
   const { token } = useAuth();
   const { classId } = useParams();
-  const [classItem, setClassItem] = useState();
-  const [teacherInfo, setTeacherInfo] = useState();
+  const [classItem, setClassItem] = useState(null);
+  const [teacherInfo, setTeacherInfo] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (token) {
-      const fetchClassItem = async () => {
+      const fetchClassAndTeacher = async () => {
         try {
+          // Fetch the class details
           const classData = await getClassDetails(classId, token);
           setClassItem(classData.class);
 
-          // Fetch teacher details
-          //   if (classData.class.createdBy) {
-          //     const teacherData = await getUserData(classData.class.createdBy, token);
-          //     setTeacherInfo(teacherData.data);
-          //   }
+          // Fetch the teacher details
+          if (classData.class.createdBy) {
+            const teacherData = await getUserData(
+              classData.class.createdBy,
+              token
+            );
+            setTeacherInfo(teacherData.data);
+          }
         } catch (error) {
           setError(error.message);
         } finally {
@@ -34,7 +40,7 @@ const ClassInfoPage = () => {
         }
       };
 
-      fetchClassItem();
+      fetchClassAndTeacher();
     } else {
       console.error('No token found, user might not be logged in.');
     }
@@ -45,63 +51,89 @@ const ClassInfoPage = () => {
   if (!classItem) return <p>Class not found.</p>;
 
   return (
-    <div className="container m-auto pt-24 justify-around ">
-      <div className="flex flex-col w-full text-black">
-        <h2 className="font-roboto font-medium text-2xl ">
+    <div className="container m-auto py-20">
+      <div className="flex flex-col w-full   justify-between text-black">
+        <h2 className="font-spartan font-medium text-4xl">
           {classItem.classTitle}
         </h2>
-      </div>
-      <div className="flex justify-around rounded-lg">
-        <div className="w-[40%] font-roboto text-xl mt-2">
-          <p>{classItem.description}</p>
-        </div>
+        <div className="flex justify-between pt-8">
+          <div className=" flex  flex-col w-[75%] md:flex-row lg:flex-row  pb-4">
+            <div className="w-[60%] font-roboto text-xl mt-2">
+              <p className="w-[95%]">{classItem.description}</p>
+            </div>
 
-        <div className="flex w-[33%]">
-          <img
-            className="rounded-lg"
-            src={classItem.classImageUrl}
-            alt={classItem.classTitle}
-          />
+            <div className="flex w-[40%]">
+              <img
+                className="rounded-lg"
+                src={classItem.classImageUrl}
+                alt={classItem.classTitle}
+              />
+            </div>
+          </div>
+          <div></div>
+          <div className="flex flex-row w-[20%] lg:flex-col text-black border-2 border-lightGreen rounded-xl m-4">
+            <div>
+              <div className="flex flex-col text-center items-center justify-center  text-black p-4 bg-lightGreen">
+                <p className="flex text-3xl items-center font-bold">
+                  ${classItem.price}
+                </p>
+                <p className="flex  text-xl  items-center font-semibold">
+                  per session
+                </p>
+              </div>
+              <div className="py-2">
+                <div className="flex items-center p-2 gap-2">
+                  <img src={AgeIcon} alt="Icon age" className="w-6 h-6" />
+                  <span>
+                    Ages: {classItem.ages.minAge} - {classItem.ages.maxAge}
+                  </span>
+                </div>
+                <div className="flex items-center p-2 gap-2">
+                  <img src={IconClock} alt="Icon clock" className="w-6 h-6" />
+                  <span>{classItem.duration} min</span>
+                </div>
+                <div className="flex items-center p-2 gap-2">
+                  <img
+                    src={LessonTypeIcon}
+                    alt="Icon age"
+                    className="w-6 h-6"
+                  />
+                  <span>{classItem.type}</span>
+                </div>
+                <div className="flex items-center p-2 gap-2  hidden md:flex">
+                  <img
+                    src={ScheduleIcon}
+                    alt="Schedule Icon"
+                    className="w-6 h-6"
+                  />
+                  <span>On Request</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-row lg:flex-col  justify-around text-black px-4 py-4 md:py-4 border-t border-gray lg:border-none">
-          <div className="flex flex-col  w-[27%] gap-4  text-black">
-            <p className="flex text-3xl font-medium items-center justify-center">
-              ${classItem.price}
-            </p>
-            <p className="text-xl flex  justify-center items-center">
-              per session
-            </p>
-          </div>
-          <div className="flex items-center gap-2">
-            <img src={AgeIcon} alt="Icon age" className="w-6 h-6" />
-            <span>
-              Ages: {classItem.ages.minAge} - {classItem.ages.maxAge}
-            </span>
-          </div>
-          <div className="flex items-center gap-2 mt-2">
-            <img src={LessonTypeIcon} alt="Icon age" className="w-6 h-6" />
-            <span>{classItem.type}</span>
-          </div>
-          <div className="flex items-center gap-2 mt-2 hidden md:flex">
-            <img src={ScheduleIcon} alt="Schedule Icon" className="w-6 h-6" />
-            <span>On Request</span>
-          </div>
-        </div>
+      </div>
+
+      <div className="mt-6">
+        <p className="text-3xl font-spartan font-medium mb-4">
+          Class experience
+        </p>
+        <p className="font-roboto text-xl w-2/3">{classItem.experience}</p>
       </div>
       <div className="mt-6">
-        <p className="text-4xl  font-medium mb-4">Class experience</p>
-        <p>{classItem.experience}</p>
+        <p className="text-3xl font-spartan font-medium  mb-4">
+          Learning Goals
+        </p>
+        <p className="font-roboto text-xl w-2/3">{classItem.goal}</p>
       </div>
       <div className="mt-6">
-        <p className="text-4xl  font-medium mb-4">LearningGoals</p>
-        <p>{classItem.goal}</p>
+        <p className="text-3xl font-spartan font-medium mb-4">Other details</p>
+        <p className="font-roboto text-xl w-2/3">{classItem.other}</p>
       </div>
       <div className="mt-6">
-        <p className="text-4xl  font-medium mb-4">Other details</p>
-        <p>{classItem.other}</p>
-      </div>
-      <div className="mt-6">
-        <p className="text-4xl font-medium mb-4">Available times</p>
+        <p className="text-3xl font-spartan font-medium mb-4">
+          Available times
+        </p>
         {classItem.availableTime && classItem.availableTime.length > 0 ? (
           classItem.availableTime.map((timeSlot) => (
             <div key={timeSlot._id}>
@@ -114,14 +146,50 @@ const ClassInfoPage = () => {
         )}
       </div>
 
-      <div>
-        {/* <div>
-              <p className="text-4xl  font-medium mb-4">Meet the teacher</p> 
-          <p>Teacher: {teacherInfo.name}</p>
-          <p>Bio: {teacherInfo.bio}</p>
-          <p>Email: {teacherInfo.email}</p>
-        </div>  */}
+      <div className="flex flex-col mt-6">
+        <p className="text-3xl font-spartan font-medium  mb-4">
+          Meet the Teacher
+        </p>
+
+        <div className="flex flex-row">
+          <img
+            src={teacherInfo.profileImageUrl}
+            alt={`${teacherInfo.firstName} ${teacherInfo.lastName}`}
+            className="w-20 h-20 rounded-full"
+          />
+          <div className="flex text-center items-center justify-center  pl-4">
+            <p className="font-roboto font-medium text-xl">{`${teacherInfo.firstName} ${teacherInfo.lastName}`}</p>
+          </div>
+        </div>
+
+        <div></div>
+
+        <div>
+          <p className="text-3xl font-spartan font-medium  my-4">
+            Teacher education and experience
+          </p>
+          <div className="flex flex-row gap-2">
+            <img src={IconDegree} alt="Icon degree" className="w-6 h-6" />
+            <p className="font-roboto text-xl">{teacherInfo.education}</p>
+          </div>
+
+          <p className="font-roboto leading-7 text-xl pt-4 w-2/3">
+            {teacherInfo.experience}
+          </p>
+          <a
+            href="#more-info"
+            className="text-black font-semibold underline hover:text-darkGreen"
+          >
+            Read More
+          </a>
+        </div>
       </div>
+      <button
+        className="bg-red hover:bg-pureWhite hover:text-red h-8 w-1/2 md:w-2/4 hover:border-2 hover:border-red text-white font-spartan font-semibold text-sm sm:text-lg rounded-lg transition duration-300 easy-in"
+        // onClick={onNavigate}
+      >
+        Edit Profile
+      </button>
     </div>
   );
 };
