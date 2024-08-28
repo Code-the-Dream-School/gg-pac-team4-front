1535d3559060f8321ca130c263aeeeb379897f35
mariyador <aniknorod@gmail.com>
2024-08-28 14:43:44 -0400

    update socketContext file

--- src/SocketContext.jsx
+++ src/SocketContext.jsx
@@ -1,46 +1,46 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import { useAuth } from './AuthProvider';
 
 const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
+  const { userData } = useAuth();
   const [socket, setSocket] = useState(null);
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    const userData = sessionStorage.getItem('user');
-    const user = userData ? JSON.parse(userData) : null;
-    const userId = user?._id;
-    
-
-    if (userId) {
-      const socketIo = io('http://localhost:8000', {
-        query: { userId: userId },
-      });
-
-      socketIo.on('connect', () => {
-        console.log('Connected with socket ID:', socketIo.id);
-      });
-
-      socketIo.on(`applications-${userId}`, (data) => {
-        console.log('Received notification:', data.content);
-      });
-
-      socketIo.on('disconnect', () => {
-        console.log('Disconnected');
-      });
-
-      setSocket(socketIo);
-
-      return () => {
-        socketIo.disconnect();
-        // Clear notifications when socket disconnects
-        setNotifications([]);
-      };
-    } else {
-      console.error('No user data found in sessionStorage');
+    if (!userData) {
+      console.error('No user data found');
+      return;
     }
-  }, []);
+
+    const userId = userData._id;
+
+    const socketIo = io('http://localhost:8000', {
+      query: { userId: userId },
+    });
+
+    socketIo.on('connect', () => {
+      console.log('Connected with socket ID:', socketIo.id);
+    });
+
+    socketIo.on(`applications-${userId}`, (data) => {
+      console.log('Received notification:', data.content);
+      setNotifications((prevNotifications) => [...prevNotifications, data.content]);
+    });
+
+    socketIo.on('disconnect', () => {
+      console.log('Disconnected');
+    });
+
+    setSocket(socketIo);
+
+    return () => {
+      socketIo.disconnect();
+      setNotifications([]); 
+    };
+  }, [userData]);
 
   return (
     <SocketContext.Provider value={{ socket, notifications }}>
