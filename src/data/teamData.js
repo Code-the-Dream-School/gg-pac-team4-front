a24079f71b7896fc86729517b838943974c187e9
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-29 22:53:25 -0700

    create file with team members data

---
+++ src/data/teamData.js
@@ -0,0 +1,21 @@
+import DanImg from '../assets/team/Dan-Polityka.jpg';
+import JoannesImg from '../assets/team/Joannes-Figueroa.jpg';
+import AnnaImg from '../assets/team/Anna-Pestova.jpg';
+import OksanaImg from '../assets/team/Oksana-Feterovskaya.jpg';
+import MariyaImg from '../assets/team/Mariya-Doronkina.jpg';
+import ValentinaImg from '../assets/team/Valentina-Rudnitskaya.png';
+import TetianaImg from '../assets/team/Tetiana-Andriyanova.jpg';
+import ElenaImg from '../assets/team/Elena-Bychenkova.jpg';
+
+const teamMembers = [
+  { src: DanImg, alt: 'Dan', name: 'Acting Skills', title: 'Full-Stack Developer' },
+  { src: JoannesImg, alt: 'Joannes Figueroa', name: 'Joannes Figueroa', title: 'Front-End Developer' },
+  { src: AnnaImg, alt: 'Anna Pestova', name: 'Anna Pestova', title: 'Full-Stack Developer' },
+  { src: OksanaImg, alt: 'Oksana Feterovskaya', name: 'Oksana Feterovskaya', title: 'Full-Stack Developer' },
+  { src: MariyaImg, alt: 'Oksana Feterovskaya', name: 'Oksana Feterovskaya', title: 'Full-Stack Developer' },
+  { src: TetianaImg, alt: 'Tetiana Andriyanova', name: 'Tetiana Andriyanova', title: 'Front-End Developer' },
+  { src: ValentinaImg, alt: 'Tetiana Andriyanova', name: 'Tetiana Andriyanova', title: 'Front-End Developer' },
+  { src: ElenaImg, alt: 'Tetiana Andriyanova', name: 'Tetiana Andriyanova', title: 'Front-End Developer' },
+];
+
+export default teamMembers;
