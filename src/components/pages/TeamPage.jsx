7160fbac766b02b34cf3b9363dacc5ad850cc8c1
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-30 21:57:55 -0700

    add mentors information

--- src/components/pages/TeamPage.jsx
+++ src/components/pages/TeamPage.jsx
@@ -13,34 +13,55 @@ const TeamPage = () => {
     <>
       <section className="flex flex-col items-center justify-center">
 
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-4xl xl:text-5xl items-center font-spartan font-semibold tracking-wide leading-tight pt-3 mb-8">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-4xl xl:text-5xl items-center font-spartan font-semibold tracking-wide leading-tight pt-3 mb-10">
           Meet Our Team
         </h1>
 
-        <h2> Team Mentors</h2>
-        <div className="w-full flex flex-col lg:flex-row items-center justify-center">
+        <h2 className="font-spartan font-semibold text-2xl lg:text-3xl"> Team Mentors</h2>
+        <div className="w-full flex flex-wrap items-center justify-center">
           <div
-            className="w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+            className="w-full h-96 basis-full md:basis-1/3 flex flex-col items-center gap-1 bg-contain bg-center bg-no-repeat"
             style={{
               backgroundImage: `url(${GreenBlobImage})`,
             }}
           >
+            <img
+              src={teamMembers[0].src}
+              alt={teamMembers[0].alt}
+              className='w-40 h-40 rounded-full border-solid border-4 border-pureWhite mt-10 ms-16'
+            />
+            <p className='w-full font-spartan font-semibold text-white text-2xl lg:text-3xl text-center ms-10'>{teamMembers[0].name}</p>
+            <p className='w-full font-spartan font-semibold text-white text-xl lg:text-2xl text-center ms-10'>{teamMembers[0].title}</p>
           </div>
 
           <div
-            className="w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+            className="w-full h-96 basis-full md:basis-1/3 flex flex-col items-center gap-1 bg-contain bg-center bg-no-repeat"
             style={{ backgroundImage: `url(${YellowBlobImage})` }}
           >
+            <img
+              src={teamMembers[1].src}
+              alt={teamMembers[1].alt}
+              className='w-40 h-40 rounded-full border-solid border-4 border-pureWhite mt-10'
+            />
+            <p className='w-full font-spartan font-semibold text-white text-2xl lg:text-3xl text-center'>{teamMembers[1].name}</p>
+            <p className='w-full font-spartan font-semibold text-white text-xl lg:text-2xl text-center'>{teamMembers[1].title}</p>
           </div>
 
           <div
-            className="w-full h-80 md:h-96 bg-contain bg-center bg-no-repeat"
+            className="w-full h-96 basis-full md:basis-1/3 flex flex-col items-center gap-1 bg-contain bg-center bg-no-repeat"
             style={{ backgroundImage: `url(${PurpleBlobImage})` }}
           >
+            <img
+              src={teamMembers[2].src}
+              alt={teamMembers[2].alt}
+              className='w-40 h-40 rounded-full border-solid border-4 border-pureWhite mt-10'
+            />
+            <p className='w-full font-spartan font-semibold text-white text-2xl lg:text-3xl text-center'>{teamMembers[2].name}</p>
+            <p className='w-full font-spartan font-semibold text-white text-xl lg:text-2xl text-center'>{teamMembers[2].title}</p>
           </div>
         </div>
 
-        <h2> Team Members</h2>
+        <h2 className="font-spartan font-semibold text-2xl lg:text-3xl">Team Members</h2>
         <div className="w-full flex flex-col lg:flex-row lg:flex-wrap items-center justify-center">
           <div
             className="lg:basis-2/6 h-80 md:h-96 bg-contain bg-center bg-no-repeat"
@@ -72,7 +93,7 @@ const TeamPage = () => {
           >
           </div>
         </div>
-      </section>
+      </section >
       <ScrollToTop />
     </>
   );
