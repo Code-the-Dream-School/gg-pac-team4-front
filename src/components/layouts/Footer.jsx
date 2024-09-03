2bde9e4645babcebaa5f417ee4361d459f534ed7
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-03 09:18:16 -0700

    add the hover effect for the link in the footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="flex items-center text-center justify-between bg-lightGreen h-16 text-black shrink-0 px-4 lg:px-16 xl:px-14 2xl:px-20">
       <p>&copy; TalentStudio</p>
       <p>Designed and developed by Team 4 for Code the Dream</p>
-      <Link to="/team">Meet the Team</Link>
+      <Link to="/team" class="hover:text-white hover:underline">Meet the Team</Link>
     </footer>
   );
 };
