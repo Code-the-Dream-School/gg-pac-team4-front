b86037698a81761d11b3b0d02ac87550e6683432
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-02 17:42:08 -0700

    update Footer, add link to the team page

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 const Footer = () => {
   return (
-    <footer className="flex flex-row items-center text-center justify-center bg-lightGreen h-16 text-black shrink-0">
-      <p>CTD Practicum Team 4</p>
+    <footer className="flex items-center text-center justify-between bg-lightGreen h-16 text-black shrink-0 px-4 lg:px-16 xl:px-14 2xl:px-20">
+      <p>&copy; TalentStudio</p>
+      <p>Designed and developed by Team 4 for Code the Dream</p>
+      <Link to="/team">Meet the Team</Link>
     </footer>
   );
 };
