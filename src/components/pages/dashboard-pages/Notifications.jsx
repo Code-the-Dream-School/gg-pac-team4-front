6e9eb1eeeee3f813ef89fec7fbb92276a8058382
Valentina <vrudnitskaya@gmail.com>
2024-09-04 11:55:56 -0500

    add conditional rendering to the heading

--- src/components/pages/dashboard-pages/Notifications.jsx
+++ src/components/pages/dashboard-pages/Notifications.jsx
@@ -1,19 +1,20 @@
-import { useState, useEffect } from 'react';
 import {
-  getClassesData,
-  getAllUsersInfo,
-  rejectApplication,
   approveApplication,
+  getAllUsersInfo,
+  getClassesData,
+  rejectApplication,
 } from '../../../util/DataBaseRequests';
-import { useAuth } from '../../../AuthProvider';
 import {
-  sortClassesByApplicationDate,
-  sortClassesByEarliestApplicationDate,
+  calculateAge,
   formatDateWithWeekday,
   formatDateWithoutWeekday,
-  calculateAge,
+  sortClassesByApplicationDate,
+  sortClassesByEarliestApplicationDate,
 } from '../../../util/NotificationsUtils';
+import { useEffect, useState } from 'react';
+
 import StudentNotifications from './StudentNotifications'
+import { useAuth } from '../../../AuthProvider';
 
 const Notifications = ({ socket }) => {
   const { userData } = useAuth();
@@ -153,9 +154,9 @@ const Notifications = ({ socket }) => {
 
   return (
     <div className="container mt-10">
-      <h1 className="text-red font-bold text-2xl sm:text-4xl font-spartan uppercase">
+      {classes.length > 0 && <h1 className="text-red font-bold text-2xl font-spartan uppercase ml-10">
         New applications:
-      </h1>
+      </h1>}
       {classesError && <p className="text-red">{classesError.message}</p>}
       <div className="flex flex-col gap-6 mt-10 m-5 md:m-10">
         {classes.length > 0 ? (
