2b2fd1f81a5fccc1c3dcc9ea911996552e1268ac
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-06 01:47:44 -0700

    create MemberCard Component and refactor Team page

--- src/components/pages/TeamPage.jsx
+++ src/components/pages/TeamPage.jsx
@@ -6,6 +6,7 @@ import YellowBlobImage from '../../assets/blob_yellow.png';
 import teamMembers from '../../data/teamData';
 import ScrollToTop from '../layouts/ScrollToTop';
 import MentorCard from './team-members-cards/MentorCard';
+import MemberCard from './team-members-cards/MemberCard';
 
 
 const TeamPage = () => {
@@ -20,66 +21,23 @@ const TeamPage = () => {
 
         <h2 className="font-spartan font-semibold text-2xl lg:text-3xl"> Team Mentors</h2>
         <div className="w-full flex flex-wrap items-center justify-center mb-4">
-          <MentorCard BlobImage={GreenBlobImage} teamMemberData={teamMembers[0]} imgShift="12" textShift="8"/>
-          <MentorCard BlobImage={YellowBlobImage} teamMemberData={teamMembers[1]} cardShift="2"/>
-          <MentorCard BlobImage={PurpleBlobImage} teamMemberData={teamMembers[2]} cardShift="6"/>
+          <MentorCard BlobImage={GreenBlobImage} teamMemberData={teamMembers[0]} cardShift="0" imgShift="24" textShift="8" />
+          <MentorCard BlobImage={YellowBlobImage} teamMemberData={teamMembers[1]} cardShift="2" />
+          <MentorCard BlobImage={PurpleBlobImage} teamMemberData={teamMembers[2]} cardShift="6" />
         </div>
 
         <h2 className="font-spartan font-semibold text-2xl lg:text-3xl mb-6">Team Members</h2>
         <div className="w-full flex flex-row flex-wrap items-center justify-center gap-10 mb-10">
-
-          <div className="w-60 basis-60 flex flex-col items-center gap-3">
-            <img
-              src={teamMembers[3].src}
-              alt={teamMembers[3].alt}
-              className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
-            />
-            <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMembers[3].name}</p>
-            <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMembers[3].title}</p>
-          </div>
-
-          <div className="w-60 basis-60 flex flex-col items-center gap-3">
-            <img
-              src={teamMembers[4].src}
-              alt={teamMembers[4].alt}
-              className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
-            />
-            <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMembers[4].name}</p>
-            <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMembers[4].title}</p>
-          </div>
+          {[...Array(teamMembers.length).keys()]
+            .filter(i => teamMembers[i].title.includes('Developer'))
+            .filter(i => teamMembers[i].title.includes('Full-Stack'))
+            .map(i => <MemberCard teamMemberData={teamMembers[i]} />)}
         </div>
-
         <div className="w-full flex flex-row flex-wrap items-center justify-center gap-10 mb-10">
-
-          <div className="w-60 basis-60 flex flex-col items-center gap-3">
-            <img
-              src={teamMembers[5].src}
-              alt={teamMembers[5].alt}
-              className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
-            />
-            <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMembers[5].name}</p>
-            <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMembers[5].title}</p>
-          </div>
-
-          <div className="w-60 basis-60 flex flex-col items-center gap-3">
-            <img
-              src={teamMembers[6].src}
-              alt={teamMembers[6].alt}
-              className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
-            />
-            <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMembers[6].name}</p>
-            <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMembers[6].title}</p>
-          </div>
-
-          <div className="w-60 basis-60 flex flex-col items-center gap-3">
-            <img
-              src={teamMembers[7].src}
-              alt={teamMembers[7].alt}
-              className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
-            />
-            <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMembers[7].name}</p>
-            <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMembers[7].title}</p>
-          </div>
+          {[...Array(teamMembers.length).keys()]
+            .filter(i => teamMembers[i].title.includes('Developer'))
+            .filter(i => teamMembers[i].title.includes('Front-End'))
+            .map(i => <MemberCard teamMemberData={teamMembers[i]} />)}
         </div>
       </section >
       <ScrollToTop />
---
+++ src/components/pages/team-members-cards/MemberCard.jsx
@@ -0,0 +1,17 @@
+const MemberCard = ({
+  teamMemberData
+}) => {
+  return (
+    <div className="w-60 basis-60 flex flex-col items-center gap-3">
+      <img
+        src={teamMemberData.src}
+        alt={teamMemberData.alt}
+        className="w-36 h-36 rounded-full border-solid border-4 border-darkGreen"
+      />
+      <p className="w-full font-spartan font-semibold text-xl lg:text-2xl text-center">{teamMemberData.name}</p>
+      <p className="w-full font-spartan font-semibold text-lg lg:text-xl text-center">{teamMemberData.title}</p>
+    </div>
+  );
+};
+
+export default MemberCard;
